0c1a8297d6e97865c02d25671087d2ac8d875884
kodaishiotsuki <0711dr5@gmail.com>
2022-06-19 18:05:42 +0900

    トップバー作成

---
+++ src/components/TopBanner/TopBanner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import "../../assets/css/custom.css";
+
+const TopBanner = () => {
+  return (
+    <>
+      <Container className='topFixedBanner'>
+        
+      </Container>
+    </>
+  );
+};
+
+export default TopBanner;
