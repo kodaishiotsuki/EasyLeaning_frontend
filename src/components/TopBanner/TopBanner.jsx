7a59a3bf123b7d0a5ae8fb400c137d5634893055
kodaishiotsuki <0711dr5@gmail.com>
2022-06-21 16:45:42 +0900

    topバー

--- src/components/TopBanner/TopBanner.jsx
+++ src/components/TopBanner/TopBanner.jsx
@@ -1,12 +1,23 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "../../assets/css/custom.css";
+// import "../../assets/css/bootstrap.min.css";
 
 const TopBanner = () => {
   return (
     <>
-      <Container className='topFixedBanner'>
-        
+      <Container className='topFixedBanner p-0' fluid={true}>
+        <div className='topBannerOverlay'>
+          <Container className='topContent'>
+            <Row>
+              <Col className='text-center'>
+                <h1>Easy Leaning</h1>
+                <h4>Learn Profesionally</h4>
+                <Button variant='primary'>Learn More</Button>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </Container>
     </>
   );
