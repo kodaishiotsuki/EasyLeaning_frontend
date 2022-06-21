db6637c5d90737675072aba77e09569013dcf7cf
kodaishiotsuki <0711dr5@gmail.com>
2022-06-21 18:45:26 +0900

    スクロール時にナビゲーションバーの背景色変更

--- src/components/TopNavgation/TopNavigation.jsx
+++ src/components/TopNavgation/TopNavigation.jsx
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from "react";
 import { Nav, Navbar } from "react-bootstrap";
+import whiteLogo from "../../assets/Image/logo_white.png";
+import blackLogo from "../../assets/Image/logo_black.png";
 
 const TopNavigation = () => {
   const [navBarTitle, setNavBarTitle] = useState("navTitle");
+  const [navBarLogo, setNavBarLogo] = useState(whiteLogo);
+  const [navBackGround, setNavBackGround] = useState("navBackground");
 
   //スクロール条件定義
   const toggleVisibility = () => {
     if (window.scrollY > 100) {
       setNavBarTitle("navTitleScroll");
+      setNavBarLogo(blackLogo);
+      setNavBackGround("navBackgroundScroll");
     } else if (window.scrollY < 100) {
       setNavBarTitle("navTitle");
+      setNavBarLogo(whiteLogo);
+      setNavBackGround("navBackground");
     }
   };
 
@@ -21,9 +29,16 @@ const TopNavigation = () => {
 
   return (
     <>
-      <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark' fixed='top'>
+      <Navbar
+        className={navBackGround}
+        collapseOnSelect
+        expand='lg'
+        bg='dark'
+        variant='dark'
+        fixed='top'
+      >
         <Navbar.Brand className={navBarTitle} href='#home'>
-          Easy Leaning
+          <img src={navBarLogo} alt='' />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls='responsive-navbar-nav' />
         <Navbar.Collapse id='responsive-navbar-nav'>
