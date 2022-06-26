b6492ce42d456614ed2ad20e3f7e5134e6773ee0
kodaishiotsuki <0711dr5@gmail.com>
2022-06-26 17:16:37 +0900

    HomePage pages Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,11 @@
 import React from "react";
-import AboutMe from "./components/AboutMe/AboutMe";
-import Analysis from "./components/Analysis/Analysis";
-import ClientReview from "./components/ClientReview/ClientReview";
-import Courses from "./components/Courses/Courses";
-import Footer from "./components/Footer/Footer";
-import RecentProject from "./components/RecentProject/RecentProject";
-import Services from "./components/Services/Services";
-import Summary from "./components/Summary/Summary";
-import TopBanner from "./components/TopBanner/TopBanner";
-import TopNavigation from "./components/TopNavgation/TopNavigation";
-import Video from "./components/Video/Video";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div>
-      <TopNavigation />
-      <TopBanner />
-      <Services />
-      <Analysis />
-      <Summary />
-      <RecentProject />
-      <Courses />
-      <Video />
-      <ClientReview />
-      <AboutMe />
-      <Footer />
-    </div>
+    <>
+      <HomePage />
+    </>
   );
 }
 
