ca9cb7283e1078f3eea30b371474f7b97a74b21d
kodaishiotsuki <0711dr5@gmail.com>
2022-06-29 16:44:56 +0900

    projectDetails component Setup

---
+++ src/components/CoursesDetails/CoursesDetails.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
+
+const CoursesDetails = () => {
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col></Col>
+        </Row>
+    </Container>
+    </>
+  )
+}
+
+export default CoursesDetails
--- src/components/Projectdetails/ProjectDetails.jsx
+++ src/components/Projectdetails/ProjectDetails.jsx
@@ -1,18 +1,65 @@
-import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import projectDetail from "../../assets/Image/pdetails.png";
+import { faCheckSquare } from "@fortawesome/free-solid-svg-icons";
 
 const ProjectDetails = () => {
   return (
     <>
-      <Container>
+      <Container className='mt-5'>
         <Row>
-          <Col>
-            <h1>Project Details</h1>
+          <Col lg={6} md={6} sm={12}>
+            <div className='about-thumb-wrap after-shape'>
+              <img src={projectDetail} alt='' />
+            </div>
+          </Col>
+          <Col lg={6} md={6} sm={12} className='mt-5'>
+            <div className='project-details'>
+              <h1 className='projectDetailsText'>
+                Education in continuing a proud tradition.
+              </h1>
+              <p className='detailsName'>
+                The quick, brown fox jumps over a lazy dog. DJs flock by when
+                MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog,
+                flick quartz, vex nymphs. Waltz, bad nymph,
+              </p>
+
+              <p className='cardSubTitle text-justify'>
+                <FontAwesomeIcon className='iconBullent' icon={faCheckSquare} />{" "}
+                Requirment Gathering{" "}
+              </p>
+
+              <p className='cardSubTitle text-justify'>
+                <FontAwesomeIcon className='iconBullent' icon={faCheckSquare} />{" "}
+                Metus interdum metus
+              </p>
+
+              <p className='cardSubTitle text-justify'>
+                <FontAwesomeIcon className='iconBullent' icon={faCheckSquare} />{" "}
+                Ligula cur maecenas Ligula cur maecenas
+              </p>
+
+              <p className='cardSubTitle text-justify'>
+                <FontAwesomeIcon className='iconBullent' icon={faCheckSquare} />{" "}
+                Ligula cur maecenasLigula cur maecenasLigula{" "}
+              </p>
+
+              <p className='cardSubTitle text-justify'>
+                <FontAwesomeIcon className='iconBullent' icon={faCheckSquare} />{" "}
+                Ligula cur maecenasLigula cur maecenasLigula cur maecenas{" "}
+              </p>
+
+              <p className='cardSubTitle text-justify'>
+                <FontAwesomeIcon className='iconBullent' icon={faCheckSquare} />{" "}
+                Ligula cur maecenasLigula cur maecenas
+              </p>
+            </div>
           </Col>
         </Row>
-    </Container>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default ProjectDetails
+export default ProjectDetails;
